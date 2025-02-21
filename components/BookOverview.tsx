65f6fc21b4b1eca3dcfa2bef8b0eb70a31158214
Vorleak Yek <vorleakyek@gmail.com>
2025-02-20 22:01:39 -0800

    update book overview

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,8 +1,44 @@
-const BookOverview = ({title, author, genre, rating,totalCopies,availableCopies, description, coverUrl} : Book) => {
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const BookOverview = ({title, author, genre, rating, totalCopies, availableCopies, description, coverUrl} : Book) => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books: <span>{totalCopies}</span>
+          </p>
+          <p>
+            Available Books: <span>{availableCopies}</span>
+          </p>
+        </div>
+
+        <p className="book-description">{description}</p>
+
+        <Button className="book-overview_btn">
+          <Image src="/icons/book.svg" alt="book" width={20} height={20}/>
+          <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
+        </Button>
+
       </div>
     </section>
   )
