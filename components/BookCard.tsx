e09f61fc9905a8ce2a5573e28300af9ca4ea8820
Vorleak Yek <vorleakyek@gmail.com>
2025-02-22 14:02:27 -0800

    Complete Home page view

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,10 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
 import BookCover from "./BookCover";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-const BookCard = ({ id, title, genre, coverColor, coverUrl, isLoanedBook = false}: Book) =>  <li>
-  <Link href={`/books/${id}`}>
+
+const BookCard = ({ id, title, genre, coverColor, coverUrl, isLoanedBook = false }: Book) => (
+<li className={cn(isLoanedBook && "xs:w-52 w-full")}>
+  <Link href={`/books/${id}`} className={cn(isLoanedBook && "w-full flex flex-col items-center")}>
+
     <BookCover coverColor={coverColor} coverImage={coverUrl}/>
+
+    <div className={cn("mt-4", !isLoanedBook && "xs:max-w-40 max-w-28")}>
+      <p className="book-title">{title}</p>
+      <p className="book-genre">{genre}</p>
+    </div>
+
+    {isLoanedBook && (
+      <div className="mt-3 w-full">
+        <div className="book-loaned">
+            <Image src="/icons/calendar.svg" alt="calendar" width={18} height={18} className="object-contain" />
+            <p className="text-light-100">11 days left to return</p>
+        </div>
+        <Button className="book-btn">Download receipt</Button>
+      </div>
+    )}
+
   </Link>
 </li>
+);
 
 export default BookCard;
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -10,12 +10,10 @@ const BookList = ({title, books, containerClassName}: Props) => {
   return (
     <section className={containerClassName}>
       <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
-
-      {books.map((book) => (
-        <BookCard key={book.title} {...book} />
-      ))}
-
       <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
 
       </ul>
     </section>
